d5c9bcacb5cded5ad28898a65fb066b60cc3fafb
ivanbiljan <ssbiljan@gmail.com>
2020-08-15 01:09:40 +0200

    Implement ApiConnection.GetResponseAsync

--- Gitmax.Lib/Http/ApiConnection.cs
+++ Gitmax.Lib/Http/ApiConnection.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Gitmax.Lib.Exceptions;
+using Gitmax.Lib.Extensions;
+using Newtonsoft.Json;
 
 namespace Gitmax.Lib.Http
 {
-    public sealed class ApiConnection
-    {
+    public sealed class ApiConnection {
         private static readonly HttpClient HttpClient;
 
         static ApiConnection() {
@@ -23,10 +29,39 @@ namespace Gitmax.Lib.Http
             };
         }
 
-        public async Task<Response> GetResponseAsync(Request request) {
+        public async Task<Response> GetResponseAsync(Request request, bool throwOnFailure = false) {
             var queryParameters = new Dictionary<string, string>();
+            var existingParameters = HttpUtility.ParseQueryString(request.Uri.Query);
+            for (var i = 0; i < existingParameters.Count; ++i) {
+                queryParameters.Add(existingParameters.GetKey(i), existingParameters.Get(i));
+            }
 
-            return null;
+            foreach (var (param, value) in request.PathParameters) {
+                queryParameters[param] = value; // Override any existing parameters
+            }
+
+            var uriBuilder = new UriBuilder(request.Uri) { Query = string.Join("&", queryParameters.Select(kvp => $"{kvp.Key}={kvp.Value}")) };
+            var requestMessage = new HttpRequestMessage(request.HttpMethod, uriBuilder.Uri);
+            foreach (var (header, value) in request.Headers) {
+                requestMessage.Headers.Add(header, value);
+            }
+
+            requestMessage.Content = request.Body switch {
+                Stream stream => new StreamContent(stream),
+                _ => new StringContent(request.Body?.ToString() ?? "", Encoding.UTF8, "applicaton/json")
+            };
+
+            var responseMessage = await HttpClient.SendAsync(requestMessage, HttpCompletionOption.ResponseContentRead);
+            var content = await responseMessage.Content.ReadAsStringAsync();
+            if (!responseMessage.IsSuccessStatusCode && throwOnFailure) {
+                // TODO: does GitHub always return a response?
+                var apiResponse = JsonConvert.DeserializeObject<ErrorResponse>(content);
+                throw new ApiErrorException(apiResponse);
+            }
+
+            var headers = responseMessage.Headers.ToDictionary(kvp => kvp.Key, kvp => string.Join(";", kvp.Value));
+            var response = new Response(responseMessage.StatusCode, headers["Content-Type"], content, headers);
+            return response;
         }
     }
 }
