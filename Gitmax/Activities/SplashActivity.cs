27e33d120cc6028d2a4a0737481205804c959cba
ivanbiljan <ssbiljan@gmail.com>
2021-02-06 18:00:31 +0100

    Add SplashActivity

---
+++ Gitmax/Activities/SplashActivity.cs
@@ -0,0 +1,54 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.Support.V7.App;
+using Android.Util;
+using Android.Nfc;
+
+namespace Gitmax.Activities
+{
+    [Activity(Theme = "@style/AppTheme.Splash", MainLauncher = true)]
+    public sealed class SplashActivity : AppCompatActivity
+    {
+        /// <inheritdoc />
+        public override void OnCreate(Bundle? savedInstanceState, PersistableBundle? persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+            Log.Debug(nameof(SplashActivity), "SplashActivity.OnCreate");
+        }
+
+        /// <inheritdoc />
+        protected override void OnResume()
+        {
+            base.OnResume();
+            var preferences = GetPreferences(FileCreationMode.Private)!;
+            if (preferences.GetString("access_token", null) is null)
+            {
+                // Launch LoginActivity
+            }
+            else
+            {
+                // Redirect to main page
+            }
+
+            //Task startupWork = new Task(SimulateStartup);
+            //startupWork.Start();
+        }
+
+        // Simulates background work that happens behind the splash screen
+        async void SimulateStartup() 
+        {
+            Log.Debug(nameof(SplashActivity), "Performing some startup work that takes a bit of time.");
+            await Task.Delay(8000); // Simulate a bit of startup work.
+            Log.Debug(nameof(SplashActivity), "Startup work is finished - starting MainActivity.");
+        }
+    }
+}
--- Gitmax/MainActivity.cs
+++ Gitmax/MainActivity.cs
@@ -10,10 +10,8 @@ using Android.Widget;
 using Java.Util.Prefs;
 
 namespace Gitmax {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    //[Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public sealed class MainActivity : AppCompatActivity {
-        internal const int LoginRequestCode = 666;
-
         protected override void OnCreate(Bundle savedInstanceState) {
         }
 
@@ -30,25 +28,7 @@ namespace Gitmax {
 
             return base.OnOptionsItemSelected(item);
         }
-
-        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data) {
-            if (requestCode != LoginRequestCode || resultCode != Result.Ok) {
-                // TODO: Do something
-            }
-
-            var preferences = GetPreferences(FileCreationMode.Private);
-            var editor = preferences.Edit();
-            editor.PutString("access_token", data.GetStringExtra("result"));
-            editor.Apply();
-            base.OnActivityResult(requestCode, resultCode, data);
-        }
-
-        private void FabOnClick(object sender, EventArgs eventArgs) {
-            View view = (View)sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (View.IOnClickListener)null).Show();
-        }
-
+        
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults) {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
