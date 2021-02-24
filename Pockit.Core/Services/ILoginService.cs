058d06ebbf2eca80b608704f81201e7823c2f958
ivanbiljan <ssbiljan@gmail.com>
2021-02-24 10:20:14 +0100

    Add LoginService

---
+++ Pockit.Core/Services/ILoginService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pockit.Core.Services 
+{
+    /// <summary>
+    /// Represents a contract that describes a login service.
+    /// </summary>
+    public interface ILoginService
+    {
+        /// <summary>
+        /// Returns a value indicating whether login information is present on the device. If so, the associated <param name="accessToken"> is returned.</param>
+        /// </summary>
+        /// <param name="accessToken">The access token. Defaults to <see langword="null"/> if no login information is present.</param>
+        /// <returns>A value indicating whether login information is present on the device.</returns>
+        bool TryGetLogin(out string? accessToken);
+    }
+}
---
+++ Pockit/Services/LoginService.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.Preferences;
+using Pockit.Core.Services;
+
+namespace Pockit.Services 
+{
+    public sealed class LoginService : ILoginService
+    {
+        private readonly ISharedPreferences _sharedPreferences;
+
+        public LoginService(ISharedPreferences sharedPreferences)
+        {
+            _sharedPreferences = sharedPreferences ?? throw new ArgumentNullException(nameof(sharedPreferences));
+        }
+
+        /// <inheritdoc />
+        public bool TryGetLogin(out string? accessToken)
+        {
+            return (accessToken = _sharedPreferences.GetString("access_token", null)) is not null;
+        }
+    }
+}
