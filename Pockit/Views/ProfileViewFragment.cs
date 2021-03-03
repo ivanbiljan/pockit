cde0a4614bcc0bf984191e9d41ec5009aa7ac0bf
ivanbiljan <ssbiljan@gmail.com>
2021-03-03 21:22:48 +0100

    Remove ImageSharp avatar mutation logic

--- Pockit/Views/ProfileViewFragment.cs
+++ Pockit/Views/ProfileViewFragment.cs
@@ -30,14 +30,7 @@ namespace Pockit.Views
 
             var httpClient = new HttpClient(new HttpClientHandler());
             await using var avatarStream = await httpClient.GetStreamAsync(ViewModel.Model.AvatarUrl);
-            using var originalImage = await Image.LoadAsync(avatarStream);
-            using var croppedImage = originalImage.Clone(x => ConvertToAvatar(x, new Size(80, 80), 5));
-
-            await using var croppedImageStream = new MemoryStream();
-            await croppedImage.SaveAsync(croppedImageStream, BmpFormat.Instance);
-
-            croppedImageStream.Position = 0;
-            _avatarView.SetImageBitmap(await BitmapFactory.DecodeStreamAsync(croppedImageStream));
+            _avatarView.SetImageBitmap(await BitmapFactory.DecodeStreamAsync(avatarStream));
         }
 
         /// <inheritdoc />
@@ -50,64 +43,6 @@ namespace Pockit.Views
             _avatarView.SetLayerType(LayerType.Software, null);
 
             return view;
-        }
-
-
-        // This method can be seen as an inline implementation of an `IImageProcessor`:
-        // (The combination of `IImageOperations.Apply()` + this could be replaced with an `IImageProcessor`)
-        private static IImageProcessingContext ApplyRoundedCorners(IImageProcessingContext ctx, float cornerRadius)
-        {
-            var size = ctx.GetCurrentSize();
-            IPathCollection corners = BuildCorners(size.Width, size.Height, cornerRadius);
-
-            ctx.SetGraphicsOptions(new GraphicsOptions
-            {
-                Antialias = true,
-                AlphaCompositionMode =
-                    PixelAlphaCompositionMode
-                        .DestOut // enforces that any part of this shape that has color is punched out of the background
-            });
-
-            // mutating in here as we already have a cloned original
-            // use any color (not Transparent), so the corners will be clipped
-            foreach (var c in corners)
-            {
-                ctx = ctx.Fill(Color.White, c);
-            }
-
-            return ctx;
-        }
-
-        private static IPathCollection BuildCorners(int imageWidth, int imageHeight, float cornerRadius)
-        {
-            // first create a square
-            var rect = new RectangularPolygon(-0.5f, -0.5f, cornerRadius, cornerRadius);
-
-            // then cut out of the square a circle so we are left with a corner
-            IPath cornerTopLeft = rect.Clip(new EllipsePolygon(cornerRadius - 0.5f, cornerRadius - 0.5f, cornerRadius));
-
-            // corner is now a corner shape positions top left
-            //lets make 3 more positioned correctly, we can do that by translating the original around the center of the image
-
-            var rightPos = imageWidth - cornerTopLeft.Bounds.Width + 1;
-            var bottomPos = imageHeight - cornerTopLeft.Bounds.Height + 1;
-
-            // move it across the width of the image - the width of the shape
-            IPath cornerTopRight = cornerTopLeft.RotateDegree(90).Translate(rightPos, 0);
-            IPath cornerBottomLeft = cornerTopLeft.RotateDegree(-90).Translate(0, bottomPos);
-            IPath cornerBottomRight = cornerTopLeft.RotateDegree(180).Translate(rightPos, bottomPos);
-
-            return new PathCollection(cornerTopLeft, cornerBottomLeft, cornerTopRight, cornerBottomRight);
-        }
-
-        // Implements a full image mutating pipeline operating on IImageProcessingContext
-        private static IImageProcessingContext ConvertToAvatar(
-            IImageProcessingContext processingContext,
-            Size size,
-            float cornerRadius)
-        {
-            return ApplyRoundedCorners(
-                processingContext.Resize(new ResizeOptions {Size = size, Mode = ResizeMode.Crop}), cornerRadius);
         }
     }
 }
