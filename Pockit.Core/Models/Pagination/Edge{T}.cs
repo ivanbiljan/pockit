31e59a762c399f74178c3cfa660cfe67c99afbc0
ivanbiljan <ssbiljan@gmail.com>
2021-03-09 20:41:24 +0100

    Define Edge<T> and IConnection<T>

---
+++ Pockit.Core/Models/Pagination/Edge{T}.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pockit.Core.Models.Pagination
+{
+    /// <summary>
+    /// Describes an edge of type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">The type this edge hosts.</typeparam>
+    public abstract class Edge<T>
+    {
+        /// <summary>
+        /// The type at the end of the edge.
+        /// </summary>
+        public T? Node { get; }
+
+        /// <summary>
+        /// A cursor for use in pagination.
+        /// </summary>
+        public string Cursor { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Edge{T}"/> class.
+        /// </summary>
+        /// <param name="node">The node at the end of the edge.</param>
+        /// <param name="cursor">A cursor for use in pagination.</param>
+        protected Edge(T? node, string cursor)
+        {
+            Node = node;
+            Cursor = cursor;
+        }
+    }
+}
---
+++ Pockit.Core/Models/Pagination/IConnection{T}.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+
+namespace Pockit.Core.Models.Pagination
+{
+    /// <summary>
+    /// Defines a contract that describes a connection of type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">The type this connection hosts.</typeparam>
+    public interface IConnection<T>
+    {
+        /// <summary>
+        /// Gets a collection od edge.
+        /// </summary>
+        ImmutableArray<Edge<T>> Edges { get; }
+        
+        /// <summary>
+        /// Gets a collection of nodes.
+        /// </summary>
+        ImmutableArray<T> Nodes { get; }
+        
+        /// <summary>
+        /// Gets information used to aid in pagination.
+        /// </summary>
+        PageInfo PageInfo { get; }
+        
+        /// <summary>
+        /// Gets the total amount of items in the connection.
+        /// </summary>
+        int TotalCount { get; }
+    }
+}
