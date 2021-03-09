5a4d85195905d46ac26eb776af814be0dc1aa44c
ivanbiljan <ssbiljan@gmail.com>
2021-03-09 20:47:29 +0100

    Define Node

---
+++ Pockit.Core/Models/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Pockit.Core.Models
+{
+    /// <summary>
+    /// Describes an object with an ID.
+    /// </summary>
+    public abstract class Node
+    {
+        /// <summary>
+        /// Gets the ID.
+        /// </summary>
+        public string Id { get; }
+    }
+}
