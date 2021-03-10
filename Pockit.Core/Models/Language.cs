3d444c67d9578b8faca90adb078a3569b0518066
ivanbiljan <ssbiljan@gmail.com>
2021-03-10 20:21:42 +0100

    Define Language

---
+++ Pockit.Core/Models/Language.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pockit.Core.Models
+{
+    /// <summary>
+    /// Represents a language found in a repository.
+    /// </summary>
+    public sealed class Language
+    {
+        /// <summary>
+        /// Gets the color defined for the language.
+        /// </summary>
+        public string Color { get; set; }
+
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
