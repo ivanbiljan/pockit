6e96ae517c044ebc43f2e5bd38c8d17605526944
ivanbiljan <ssbiljan@gmail.com>
2021-03-10 20:35:55 +0100

    Define RepositoryAffiliation

---
+++ Pockit.Core/Models/RepositoryAffiliation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pockit.Core.Models
+{
+    /// <summary>
+    /// Specifies a user's affiliation to a repository.
+    /// </summary>
+    public enum RepositoryAffiliation
+    {
+        /// <summary>
+        /// Indicates that the user is a collaborator.
+        /// </summary>
+        Collaborator,
+
+        /// <summary>
+        /// Indicates that the user has access to a repository through being a member of an owner organization.
+        /// </summary>
+        OrganizationMember,
+
+        /// <summary>
+        /// Indicates that the repository is owned by the authenticated user.
+        /// </summary>
+        Owner
+    }
+}
