851d19cf0d107fda39e4cc05ba8b51cb12b5e66a
ivanbiljan <ssbiljan@gmail.com>
2021-03-17 12:17:06 +0100

    Add RepositoryViewModel

---
+++ Pockit/ViewModels/RepositoryViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Pockit.Core.Models;
+
+namespace Pockit.ViewModels {
+    public sealed class RepositoryViewModel : ViewModelBase<Repository> {
+        public Repository Model { get; private set; }
+        
+        /// <inheritdoc />
+        public override void Prepare(Repository parameter)
+        {
+            Model = parameter;
+        }
+    }
+}
