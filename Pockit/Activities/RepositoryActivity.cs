82fdd2fa3097a77a9bf9c55fa76fbdedd2ebc901
ivanbiljan <ssbiljan@gmail.com>
2021-03-17 17:54:37 +0100

    Add RepositoryActivity and RepositoryActivityViewModel

---
+++ Pockit/Activities/RepositoryActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MvvmCross.Platforms.Android.Presenters.Attributes;
+using MvvmCross.Platforms.Android.Views;
+using Pockit.ViewModels;
+
+namespace Pockit.Activities
+{
+    [Activity]
+    [MvxActivityPresentation]
+    public sealed class RepositoryActivity : MvxActivity<RepositoryActivityViewModel>
+    {
+        /// <inheritdoc />
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.RepositoryActivityView);
+        }
+    }
+}
---
+++ Pockit/ViewModels/RepositoryActivityViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MvvmCross.Commands;
+using MvvmCross.Navigation;
+using Pockit.Core.Models;
+
+namespace Pockit.ViewModels
+{
+    public sealed class RepositoryActivityViewModel : ViewModelBase<Repository>
+    {
+        private readonly IMvxNavigationService _navigationService;
+
+        public RepositoryActivityViewModel(IMvxNavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public Repository Model { get; private set; } = null!;
+
+        /// <inheritdoc />
+        public override void Prepare(Repository parameter)
+        {
+            Model = parameter;
+        }
+    }
+}
